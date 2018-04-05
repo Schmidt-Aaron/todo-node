d6d41e1693859b2b883c1dbd236a7beed5eb4f5a
Schmidt-Aaron <aschmidt808@gmail.com>
2018-04-05 10:04:05 -0700

    add delete function

--- public/app.js
+++ public/app.js
@@ -1,26 +1,6 @@
 const todoInput = document.querySelector('#todoInput');
 const todoList = document.querySelector('.list');
 
-//parse entire todo list
-const populateTodos = (data) => {
-  data.forEach( (todo) => {
-    postTodo(todo);  
-  })
-}
-
-//add single todo to page
-const postTodo = (todo) => {
-  let listItem = document.createElement('li');
-  let newTodo = `${todo.name} <span class="delete">X</span>`;
-  
-  listItem.innerHTML = newTodo;
-  if(todo.completed){
-    listItem.classList.add('completed')
-  }
-
-  todoList.appendChild(listItem);
-}
-
 //ping API for todos
 const getTodos = () => {
   axios.get('/api/todos')
@@ -31,6 +11,28 @@ const getTodos = () => {
       console.log(err);
     })
 }
+
+//parse initial API data
+const populateTodos = (data) => {
+  data.forEach( (todo) => {
+    postTodo(todo);  
+  })
+}
+
+//add single todo to page
+const postTodo = (todo) => {
+  let listItem = document.createElement('li');
+  listItem.setAttribute('data-ID', todo._id);
+  let newTodo = `${todo.name} <span class="delete">X</span>`;
+  
+  listItem.innerHTML = newTodo;
+  if(todo.completed){
+    listItem.classList.add('completed')
+  }
+
+  todoList.appendChild(listItem);
+}
+
 
 //creat a new todo
 const createTodo = () => {
@@ -45,14 +47,29 @@ const createTodo = () => {
   .catch(err => console.log(err));
 }
 
+//handle entries
 todoInput.addEventListener('keypress', (e) => {
   if(e.charCode === 13) {
     createTodo();
   }
 })
 
-todoList.addEventListener('click', () => {
-  console.log('click')
+//handle item deletions
+todoList.addEventListener('click', (e) => {
+  if(e.target.className === 'delete') {
+    e.target.parentElement.parentElement.removeChild(e.target.parentElement);
+
+    deleteTodo(e.target.parentElement);
+  }
 });
 
+//API call to delete item
+const deleteTodo = todo => {
+  let ID = todo.getAttribute('data-ID');
+  axios.delete(`/api/todos/${ID}`)
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
+}
+
+//populate list on window load
 window.addEventListener('load', getTodos);
